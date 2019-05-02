7ddd31b6cdedfe70531b9355b4630f24abe4b476
Mohamed Fawzan <fawzanm@gmail.com>
2019-05-02 17:27:01 +0530

    Fixes typo in a joke

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -7,7 +7,7 @@ class JokeFactory
 
     protected $jokes = [
 
-        'Chuck Norris threw a grenade and killed 50 people, then it exploded.',
+        'Chuck Norris threw a grenade and killed 50 people, then it exploded...',
         'When Chuck Norris was in middle school, his English teacher assigned an essay: "What is courage?" He received an A+ for turning in a blank page with only his name at the top.',
         'Chuck Norris can kill two stones with one bird.',
         'Chuck Norris knows Victoria\'s secret.'
